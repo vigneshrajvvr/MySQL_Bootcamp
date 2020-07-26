d3d251eb2ce0af7a11bce14d067cd5e62abccc8f
root <vvigneshrajvvr@gmail.com>
2020-07-26 14:58:45 +0000

    One to One Relationship;

--- Relationships/CustomersOrders.sql
+++ Relationships/CustomersOrders.sql
@@ -40,3 +40,88 @@ SELECT * FROM orders WHERE customer_id =
         SELECT id FROM customers 
         WHERE last_name = 'George' 
     ); 
+    
+-- IMPLICIT INNER JOIN
+SELECT * FROM customers,orders 
+WHERE customers.id = customer_id;
+
+SELECT customers.id,first_name,last_name, order_date, amount FROM customers,orders 
+WHERE customers.id = customer_id;
+
+
+--EXPLICIT INNER JOIN
+SELECT * FROM customers
+JOIN orders
+    ON customers.id = orders.customer_id;
+    
+SELECT first_name, last_name, order_date, amount FROM customers
+JOIN orders
+    ON customers.id = orders.customer_id;
+    
+-- SELECT ORDER SWITCHED
+SELECT * FROM orders
+JOIN customers
+    ON customers.id = orders.customer_id;
+    
+-- ARBITARY JOIN - don't do this!git stat
+SELECT * FROM customers
+JOIN orders
+    ON customers.id = orders.id;
+    
+-- Getting advanced
+SELECT customers.id,first_name, last_name, order_date, amount FROM customers
+JOIN orders
+    ON customers.id = orders.customer_id
+    ORDER BY order_date;
+    
+SELECT 
+    customers.id,
+    COUNT(*) AS order_count, 
+    first_name, 
+    last_name, 
+    order_date, 
+    SUM(amount) AS total_spent 
+FROM customers
+JOIN orders
+    ON customers.id = orders.customer_id
+GROUP BY orders.customer_id
+ORDER BY total_spent DESC;
+
+-- LEFT JOIN
+SELECT customers.id,first_name, last_name, order_date, amount FROM customers
+LEFT JOIN orders
+    ON customers.id = orders.customer_id;
+    
+SELECT 
+    customers.id,
+    first_name, 
+    last_name, 
+    IFNULL(SUM(amount), 0) AS total_spent
+FROM customers
+LEFT JOIN orders
+    ON customers.id = orders.customer_id
+GROUP BY customer_id
+ORDER BY total_spent DESC;
+
+--RIGHT JOIN
+SELECT customers.id,first_name, last_name, order_date, amount FROM customers
+RIGHT JOIN orders
+    ON customers.id = orders.customer_id;
+    
+-- ON DELETE CASECASE
+CREATE TABLE customers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),    
+    email VARCHAR(100)
+);
+
+
+CREATE TABLE orders(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY(customer_id) 
+        REFERENCES customers(id) ON DELETE CASECADE
+);
