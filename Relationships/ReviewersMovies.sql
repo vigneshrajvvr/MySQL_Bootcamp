7cd06cda7b34e0efcc93274d2559fa419f99a1e4
root <vvigneshrajvvr@gmail.com>
2020-08-17 18:41:06 +0000

    Many to Many challenges progress

--- Relationships/ReviewersMovies.sql
+++ Relationships/ReviewersMovies.sql
@@ -84,4 +84,44 @@ JOIN reviews
 SELECT title AS unreviewed_series FROM series
 LEFT JOIN reviews
     ON series.id = reviews.series_id
-WHERE rating IS NULL;    
+WHERE rating IS NULL; 
+
+-- Challenge 5
+
+SELECT genre, ROUND(
+        AVG(rating),2
+       ) AS avg_rating FROM series
+JOIN reviews 
+    ON series.id = reviews.series_id
+GROUP BY genre;
+
+-- Challenge 6
+
+SELECT first_name,
+       last_name, 
+       COUNT(rating) AS COUNT, 
+       IFNULL(MIN(rating),0) AS MIN, 
+       IFNULL(MAX(rating),0) AS MAX, 
+       IFNULL(AVG(rating),0) as AVG,
+       CASE
+           WHEN COUNT(rating) > 0 THEN 'ACTIVE'
+           ELSE 'INACTIVE'               
+       END AS STATUS
+FROM reviewers 
+LEFT JOIN reviews
+    ON reviewers.id = reviews.reviewer_id
+GROUP BY reviewers.id;
+
+-- Another way
+
+SELECT first_name,
+       last_name, 
+       COUNT(rating) AS COUNT, 
+       IFNULL(MIN(rating),0) AS MIN, 
+       IFNULL(MAX(rating),0) AS MAX, 
+       IFNULL(AVG(rating),0) as AVG,
+	   IF(COUNT(rating) > 0, 'ACTIVE','INACTIVE') AS STATUS
+FROM reviewers 
+LEFT JOIN reviews
+    ON reviewers.id = reviews.reviewer_id
+GROUP BY reviewers.id;
---
+++ Relationships/StudentsPapers_copy0.sql
@@ -0,0 +1,75 @@
+# CREATE THE SCHEMA
+CREATE TABLE students(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100)
+);
+
+CREATE TABLE papers(
+    title VARCHAR(100),
+    grade INT,
+    student_id INT,
+    FOREIGN KEY (student_id) 
+        REFERE    NCES students(id) 
+        ON DELETE CASCADE
+);
+
+INSERT INTO students (first_name) VALUES 
+('Caleb'), ('Samantha'), ('Raj'), ('Carlos'), ('Lisa');
+
+INSERT INTO papers (student_id, title, grade ) VALUES
+(1, 'My First Book Report', 60),
+(1, 'My Second Book Report', 75),
+(2, 'Russian Lit Through The Ages', 94),
+(2, 'De Montaigne and The Art of The Essay', 98),
+(4, 'Borges and Magical Realism', 89);
+
+-- EXCERCISE 1
+SELECT first_name, title, grade FROM students
+INNER JOIN papers 
+    ON students.id = papers.student_id
+ORDER BY grade DESC;
+
+-- ALTERNATIVE SOLUTION
+SELECT first_name, title, grade FROM students
+RIGHT JOIN papers 
+    ON students.id = papers.student_id
+ORDER BY grade DESC;
+
+-- EXCERCISE 2       
+SELECT first_name, title, grade FROM students
+LEFT JOIN papers 
+    ON students.id = papers.student_id;
+    
+-- EXCERCISE 3
+SELECT 
+    first_name, 
+    IFNULL(title,'MISSING'), 
+    IFNULL(grade,0) 
+FROM students
+LEFT JOIN papers 
+    ON students.id = papers.student_id;  
+    
+-- EXCERCISE 4
+SELECT     
+    first_name,
+    IFNULL(AVG(grade),0) AS average
+FROM students
+LEFT JOIN papers
+    ON students.id = papers.student_id
+GROUP BY students.id
+ORDER BY average DESC;
+
+-- EXCERCISE 5
+SELECT     
+    first_name ,
+    IFNULL(AVG(grade),0) AS average,
+    CASE
+        WHEN AVG(grade) IS NULL THEN 'FAILING'
+        WHEN AVG(grade) >= 75 THEN 'PASSING'
+        ELSE 'FAILING'
+    END AS passing_status
+FROM students
+LEFT JOIN papers
+    ON students.id = papers.student_id
+GROUP BY students.id
+ORDER BY average DESC;
