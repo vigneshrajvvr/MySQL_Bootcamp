6f2f9e5a34788afdd9042be77ae44b3a0ed5518b
root <vvigneshrajvvr@gmail.com>
2020-08-30 18:44:35 +0000

    instagram tables

--- instagram/ig_clone.sql
+++ instagram/ig_clone.sql
@@ -32,9 +32,31 @@ CREATE TABLE comment (
 CREATE TABLE likes (
     user_id INT NOT NULL,
     photo_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURR(),
+    created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY(user_id) REFERENCES user(id),
     FOREIGN KEY(photo_id) REFERENCES photo(id),
     PRIMARY KEY(user_id, photo_id)
 );
 
+CREATE TABLE follow (
+    follower_id INT,
+    followee_id INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES user(id),
+    FOREIGN KEY(followee_id) REFERENCES user(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+CREATE TABLE tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+  
+CREATE TABLE photo_tag (
+    photo_id INT NOT NULL,
+    tag_id INT NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photo(id),
+    FOREIGN KEY(tag_id) REFERENCES tag(id),
+    PRIMARY KEY (photo_id, tag_id)
+);
